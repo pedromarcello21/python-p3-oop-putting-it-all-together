e498fa20b492035aa94c4dad7dc08954728c6223
Pedro Marcello Vincenty <vincentypedro@gmail.com>
2024-07-16 16:15:46 -0400

    first commit

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,41 @@
 #!/usr/bin/env python3
 
 class Book:
+
+    def __init__(self, title, page_count, page_reading):
+        self.page_count = page_count
+        self.title = title
+        self.page_reading = page_reading
+        
+    def __repr__(self):
+        return f"I'm reading {self.title}. It's {self.page_count} pages long and I\'m currently on page {self.page_reading}"
+    
+    @property
+    def page_count(self):
+        return self._page_count
+    
+    @page_count.setter
+    def page_count(self, value):
+        if not isinstance(value, int):
+            raise AssertionError("page_count must be an integer")
+        else:
+            self._page_count=value
+
+    @property
+    def page_reading(self):
+        return self._page_reading
+    
+    @page_reading.setter
+    def page_reading(self, value):
+        if not isinstance(value, int):
+            raise AssertionError("page_reading must be an integer")
+        else:
+            self._page_reading=value
+
+    
+    def turn_page(self):
+        self.page_reading +=1
+    
     pass
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,28 @@
 #!/usr/bin/env python3
 
 class Shoe:
+    def __init__(self, brand, size):
+        # assert type(size)==int, "size must be an integer"
+        self.brand = brand
+        self.size = size
+
+
+    @property
+    def size(self):
+        return self._size
+    
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise AssertionError("size must be an integer")
+        else:
+            self._size=value
+    
+    def __repr__(self):
+        return f"I have a pair of {self.brand} size {self.size}"
+    
+    def cobble(self):
+        self.condition = "New"
+        return f"Your {self.brand} are as good as new!"
+
     pass
